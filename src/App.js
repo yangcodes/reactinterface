b6d8746a8126cbc6643d00bf9e27d717e0f9fc8b
Jenny Lin <misslinyang91@gmail.com>
2021-08-03 12:58:47 +1000

    added appointmentinfo and search form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { BiArchive } from "react-icons/bi";
+import { BiCalendar } from "react-icons/bi";
 import Search from "./components/Search";
-
+import AddAppointment from "./components/AddAppointment";
+import AppointmentInfo from "./components/Appointmentinfo";
+import appointmentList from "./data.json";
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top" />
+      <h1 className="text-5xl mb-3">
+        <BiCalendar className="inline-block text-red-400 align-top" />
         Your Appointments
+        <AddAppointment />
         <Search />
+        <ul className="divide-y divide-gray-200">
+          {appointmentList.map((appointment) => (
+            <AppointmentInfo key={appointment.id} appointment={appointment} />
+          ))}
+        </ul>
       </h1>
     </div>
   );
