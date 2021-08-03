dd359836350bc87ae2fbde9cbd3e09bc24f77778
Jenny Lin <misslinyang91@gmail.com>
2021-08-03 11:49:18 +1000

    added form and dropdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { BiArchive } from "react-icons/bi";
+import Search from "./components/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <BiArchive className="inline-block text-red-400 align-top" />
+        Your Appointments
+        <Search />
+      </h1>
     </div>
   );
 }
